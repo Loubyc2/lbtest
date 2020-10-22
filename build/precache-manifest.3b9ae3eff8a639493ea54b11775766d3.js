d4024a732c84f08892fcc26ed18ce331ef38a075
Louby <loubyc2@gmail.com>
2020-10-21 21:51:24 -0300

    first commit

---
+++ build/precache-manifest.3b9ae3eff8a639493ea54b11775766d3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d63e4ae5532b1f3be45f1161cf428e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51d12ef1c717e9b44e94",
+    "url": "/static/css/main.edaa7a60.chunk.css"
+  },
+  {
+    "revision": "0f2b617bf6db8df6cbd6",
+    "url": "/static/js/2.2f1cc40d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2f1cc40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d12ef1c717e9b44e94",
+    "url": "/static/js/main.5316f13a.chunk.js"
+  },
+  {
+    "revision": "14480e30beecc6397ad4",
+    "url": "/static/js/runtime-main.704043c5.js"
+  },
+  {
+    "revision": "5114e1616ba027e46601f898a596e885",
+    "url": "/static/media/banner-1.5114e161.jpg"
+  },
+  {
+    "revision": "a14e14e284a4fa3d1df4155bf8a6ec22",
+    "url": "/static/media/product-1.a14e14e2.png"
+  },
+  {
+    "revision": "2052d9f2baf385649fd4dc6a14576136",
+    "url": "/static/media/product-11.2052d9f2.png"
+  },
+  {
+    "revision": "eed517c90e23e9046bd3cd40efbae6fc",
+    "url": "/static/media/product-12.eed517c9.png"
+  },
+  {
+    "revision": "fd81b50653025a673269fb510fe2354d",
+    "url": "/static/media/product-14.fd81b506.png"
+  },
+  {
+    "revision": "7ba43a4f220b1a283bb55bea937cecee",
+    "url": "/static/media/product-17.7ba43a4f.png"
+  },
+  {
+    "revision": "26c8dc6316e0df4fca60cf087f5b61f2",
+    "url": "/static/media/product-2.26c8dc63.png"
+  },
+  {
+    "revision": "3c110f8cb6353ff3513e3604b7120940",
+    "url": "/static/media/product-3.3c110f8c.png"
+  },
+  {
+    "revision": "2e18556efdbcbe97f79fee19c0597db5",
+    "url": "/static/media/product-4.2e18556e.png"
+  },
+  {
+    "revision": "3c1fb6efce4bebba4890651838e207e2",
+    "url": "/static/media/product-5.3c1fb6ef.png"
+  },
+  {
+    "revision": "575e94fc5c7f857c9f83a6a6c05c8f86",
+    "url": "/static/media/product-9.575e94fc.png"
+  }
+]);
